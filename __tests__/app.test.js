2e552154a04c9cdc5b3da329b8d9d175ca54f797
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-10-18 12:56:07 -0700

    tests are passing, for got to acp a bunch

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,15 +2,127 @@ const pool = require('../lib/utils/pool.js');
 const setup = require('../data/setup.js');
 const request = require('supertest');
 const app = require('../lib/app.js');
+const UserService = require('../lib/services/UserServ.js');
+
+
 
 describe('alchemy-app routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it('post a new user to table Users', async () => {
-    return request(app).post('/auth/signup').then();
+ 
+
+  it('posts new user to DB', async () => {
+    const res = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'test@email.com',
+        password: 'fake-password',
+        role: 'PALADIN'
+      });
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email: 'test@email.com',
+      role: 'PALADIN'
+    });
   });
+
+
+
+  it('checks for existing users', async () => {
+    await UserService.createUser({
+      email: 'test@email.com',
+      password: 'fake-password',
+      roleTitle: 'PALADIN'
+    });
+ 
+    const res = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'test@email.com',
+        password: 'fake-password',
+        roleTitle: 'PALADIN'
+      });
+    
+    expect(res.body).toEqual({
+      message: 'User already exists',
+      status: 400
+    });
+  });
+
+  it('logs existing user in via post route', async () => {
+    // jest.setTimeout(10000);
+    await UserService.createUser({
+      email: 'test@email.com',
+      password: 'fake-password',
+      roleTitle: 'PALADIN'
+    });
+    const res = await request(app)
+      .post('/api/v1/auth/login')
+      .send({ 
+        email: 'test@email.com',
+        password:'fake-password',
+        roleTitle: 'PALADIN'
+      });
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email: 'test@email.com',
+      role: 'PALADIN'
+    });
+  });
+
+  it('ensures login credentials exist in DB', async () => {
+    await UserService.createUser({
+      email: 'test@email.com',
+      password: 'fake-password',
+      roleTitle: 'MAGE'
+    });
+
+    const res = await request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'test@email.com',
+        password:'password',
+        roleTitle: 'MAGE' 
+      });
+   
+    expect(res.body).toEqual({
+      message: 'Invalid email/password',
+      status: 401 }
+    );
+  });
+
+  it('gets user currently logged in using cookie at /me', async () => {
+    await UserService.createUser({
+      email: 'test@email.com',
+      password: 'fake-password',
+      roleTitle: 'MAGE'
+    });
+
+    const agent = await request.agent(app);
+
+    await agent
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'test@email.com',
+        password:'fake-password',
+        roleTitle: 'MAGE'
+      });
+
+    const res = await agent
+      .get('/api/v1/auth/me');
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      exp: expect.any(Number),
+      iat: expect.any(Number),
+      email: 'test@email.com',
+      role: 'MAGE'
+    });
+  });
+
 
   afterAll(() => {
     pool.end();
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,8 +1,24 @@
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS roles;
 
+CREATE TABLE roles (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL UNIQUE
+);
 
 CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    email TEXT NOT NULL UNIQUE,
-    password_hash TEXT NOT NULL
+    email TEXT NOT NULL,
+    password_hash TEXT NOT NULL,
+    role_id BIGINT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES roles(id)
 );
+
+
+
+INSERT INTO roles (title)
+VALUES ('PALADIN'), ('MAGE'), ('BARD'), ('DRUID'), ('CLERIC');
+
+INSERT INTO users (email, password_hash, role_id)
+VALUES ('ilovecookie@me.com', 'password', '1')
+RETURNING *;
